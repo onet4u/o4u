1fbdd3ee61af6e0bafaf93c8af5d5284d5097f9b
nazrenv2 <49838905+nazrenv2@users.noreply.github.com>
2019-04-23 18:54:07 +0800

    Update 123.js

--- 123.js
+++ 123.js
@@ -1,3 +1,9 @@
-!function(){function f(){var a=document.createElement("div");a.id="arlinablock";a.innerHTML='<div class="inner"><div class="header"><h2>Ad Blocker Detected</h2></div> <div class="isi"> <p>Please consider supporting us by disabling your ad blocker</p>Refresh the page if it didn\'t refresh automatically. Thanks!</div></div>';
+!function(){function f(){var a=document.createElement("div");a.id="arlinablock";a.innerHTML='<div id='keep-ads'>
+<div class='keep-adsme'>
+<p><h4>Adblock Detected</h4></p>
+<p><span><i aria-hidden='true' class='fa fa-expeditedssl'></i></span></p>
+<p>Like this blog? Keep us running by whitelisting this blog in your ad blocker</p>
+<p>This is <a href='https://www.gobeatz.com/your-whitelist-page-contect' target='_blank' title='How to Whitelist'><i>how to whitelisting</i></a> this blog in your ad blocker</p>
+<p>Thank you</p></div></div>';
 document.body.append(a);document.body.style.overflow="hidden";var b=a.querySelectorAll("button");a.querySelector(".close");var d=a.querySelectorAll(".fixblock > div");for(a=0;a<b.length;a++)b[a].addEventListener("click",function(a){a.preventDefault();a=this.getAttribute("class").split(" ")[0];for(var c=0;c<d.length;c++)d[c].classList.remove("active"),b[c].classList.remove("active");b[a-1].classList.add("active");d[a-1].classList.add("active")})}var b=document.createElement("script");b.type="text/javascript";
 b.async=!0;b.src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";b.onerror=function(){f();window.adblock=!0};var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(b,e)}();
